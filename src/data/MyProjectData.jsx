98274f72195ff3bcbc9907d73915f6bde5e1ca33
Nikola Zovko <nikolajoe95@gmail.com>
2024-02-15 14:31:30 +0100

    noviupdajte4

--- src/data/MyProjectData.jsx
+++ src/data/MyProjectData.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 //img projekts
 import Projekt1 from '../assets/work-img/work-5.png';
-import Projekt2 from '../assets/work-img/work-1.png';
+import Projekt2 from '../assets/work-img/Task-Todo-RJ.png';
+import ComingSoon from '../assets/work-img/work-1.png';
 
 // skils img 
 import Html from '../assets/images/icons-html.png';
@@ -18,7 +19,7 @@ import Figma from '../assets/images/icon-Figma.gif';
 const MyProjectData = [
     {
         id: 0,
-        title: "Chat App React Native",
+        title: "Chat App, React Native",
         ImgProject: Projekt1,
         description: "The possibility of creating a personal account, Log in, Log Out. Chat with others in real time. Possibility of changing the username, password and deleting the existing account. It is also possible to set a picture for the profile.",
         skills: [Html, Css, JavaScript, ReactNative],
@@ -27,17 +28,17 @@ const MyProjectData = [
     },
     {
         id: 1,
-        title: "Coming Soon",
+        title: "Task To-Do, React JS",
         ImgProject: Projekt2,
-        description: "Description for another project...",
-        skills: [Html, Css, JavaScript],
-        demo: "",
-        source: "https://github.com/Nikolaz95/Another-project"
+        description: "Task To-Do, ReactJS App with Local Storage. You can store all your Tasks.",
+        skills: [Html, Css, JavaScript, ReactJS],
+        demo: "https://task-to-do-nikolazovko.netlify.app/",
+        source: "https://github.com/Nikolaz95/todoRJ"
     },
     {
         id: 2,
         title: "Coming Soon",
-        ImgProject: Projekt2,
+        ImgProject: ComingSoon,
         description: "Description for another project...",
         skills: [Html, Css, JavaScript],
         demo: "",
@@ -46,7 +47,7 @@ const MyProjectData = [
     {
         id: 3,
         title: "Coming Soon",
-        ImgProject: Projekt2,
+        ImgProject: ComingSoon,
         description: "Description for another project...",
         skills: [Html, Css, JavaScript],
         demo: "",
@@ -55,7 +56,7 @@ const MyProjectData = [
     {
         id: 4,
         title: "Coming Soon",
-        ImgProject: Projekt2,
+        ImgProject: ComingSoon,
         description: "Description for another project...",
         skills: [Html, Css, JavaScript],
         demo: "",
